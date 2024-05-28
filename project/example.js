20b31533eb64b8426a546c4df4f2d0830ee12b3f
Shinkiti-1 <abdullahiismailadeboye@gmail.com>
2024-05-28 12:26:37 -0100

    second project

---
+++ project/example.js
@@ -0,0 +1,17 @@
+
+// let myName = "2"
+// console.log(myName);
+//     myName=parseInt(myName)
+//     console.log(myName);
+//     word = 5
+//     result = myName === word
+// console.log(typeof(myName));
+// console.log(result);
+
+
+// let
+// var
+// const
+
+
+
